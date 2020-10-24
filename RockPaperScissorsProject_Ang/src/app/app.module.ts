d7c54d1faedb267378d8eb4b23be44c99d2e173a
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-10-24 22:17:21 +1100

    Revert "Modified API Inputs/outputs. Amended Startup.cs"
    This reverts commit 68cb50e68a80844619687355ed813d5846f9105d.

--- RockPaperScissorsProject_Ang/src/app/app.module.ts
+++ RockPaperScissorsProject_Ang/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SelectionComponent } from './routes/selection/selection.component';
 import { ResultComponent } from './routes/result/result.component';
-import { HttpClientModule} from '@angular/common/http';
-
-// HttpClientModule has to be imported here in order 
-// for the app to be able to pass info to a remote client
 
 @NgModule({
   declarations: [
@@ -15,11 +12,9 @@ import { HttpClientModule} from '@angular/common/http';
     SelectionComponent,
     ResultComponent
   ],
-
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- RockPaperScissorsProject_Ang/src/app/routes/result/result.component.ts
+++ RockPaperScissorsProject_Ang/src/app/routes/result/result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { SelectionService } from "../../services/selection.service";
 
 @Component({
@@ -9,18 +10,55 @@ import { SelectionService } from "../../services/selection.service";
 
 export class ResultComponent implements OnInit {
 
+
+  playerwins?: boolean = null;
+
   constructor(public selectionService: SelectionService) { }
 
   ngOnInit(): void {
+
+    //player win
+    if (this.selectionService.selection == 'rock' && this.selectionService._aiselection == 'scissors') {
+      this.playerwins = true;
+    }
+    if (this.selectionService.selection == 'paper' && this.selectionService._aiselection == 'rock') {
+      this.playerwins = true;
+    }
+    if (this.selectionService.selection == 'scissors' && this.selectionService._aiselection == 'paper') {
+      this.playerwins = true;
+    }
+
+    //AI win
+    if (this.selectionService._aiselection == 'rock' && this.selectionService.selection == 'scissors') {
+      this.playerwins = false;
+    }
+    if (this.selectionService._aiselection == 'paper' && this.selectionService.selection == 'rock') {
+      this.playerwins = false;
+    }
+    if (this.selectionService._aiselection == 'scissors' && this.selectionService.selection == 'paper') {
+      this.playerwins = false;
+    }
+
+    //DRAW
+    if (this.selectionService._aiselection == 'rock' && this.selectionService.selection == 'rock') {
+      this.playerwins = null;
+    }
+    if (this.selectionService._aiselection == 'paper' && this.selectionService.selection == 'paper') {
+      this.playerwins = null;
+    }
+    if (this.selectionService._aiselection == 'scissors' && this.selectionService.selection == 'scissors') {
+      this.playerwins = null;
+    }
+
   }
 
-  showhidedevinfo() {
-    if (this.selectionService.showHideDevInfo == false) {
+  showhidebutton() {
+    if (this.selectionService.showhideoptions == false) {
       console.log("option menu shown");
-      this.selectionService.showHideDevInfo = true;
-    } else if (this.selectionService.showHideDevInfo == true) {
+      this.selectionService.showhideoptions = true;
+    } else if (this.selectionService.showhideoptions == true) {
       console.log("option menu hidden");
-      this.selectionService.showHideDevInfo = false;
+      this.selectionService.showhideoptions = false;
     }
   }
 

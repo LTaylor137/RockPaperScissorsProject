a2b0e9199769fa1dead3a418eb8c7eb72ef216b5
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-10-24 22:16:24 +1100

    merge before trying to revert.

--- RockPaperScissorsProject_Ang/src/app/routes/selection/selection.component.ts
+++ RockPaperScissorsProject_Ang/src/app/routes/selection/selection.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SelectionService } from "../../services/selection.service";
+import { style } from '@angular/animations';
 
 @Component({
-  selector: 'app-colour-picker',
+  selector: 'app-selection',
   templateUrl: './selection.component.html',
   styleUrls: ['./selection.component.css']
 })
@@ -14,19 +15,39 @@ export class SelectionComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  selectOption(option: 'rock' | 'paper' | 'scissors'){
-    this.selectionService.commitSelection(option);
+  playerSelection(playerchoice: 'rock' | 'paper' | 'scissors') {
+    this.selectionService.setSelection(playerchoice);
+
+    //the below highlights the selected option==========================
+    document.getElementById("rock").classList.remove('boxselected');
+    document.getElementById("paper").classList.remove('boxselected');
+    document.getElementById("scissors").classList.remove('boxselected');
+    if (playerchoice === 'rock') {
+      document.getElementById("rock").classList.add('boxselected');
+    } 
+    if (playerchoice === 'paper') {
+      document.getElementById("paper").classList.add('boxselected');
+    } 
+    if (playerchoice === 'scissors') {
+      document.getElementById("scissors").classList.add('boxselected');
+    }
+    //==================================================================
   }
 
-  showhidebutton(){
-      if (this.selectionService.showhideoptions == false){
-        console.log("option menu shown");
-        this.selectionService.showhideoptions = true;
-      } else if (this.selectionService.showhideoptions == true){
-        console.log("option menu hidden");
-        this.selectionService.showhideoptions = false;
-      } 
+  shoot() {
+    this.selectionService.commitSelection();
   }
+
+  showhidedevinfo() {
+    if (this.selectionService.showHideDevInfo == false) {
+      console.log("option menu shown");
+      this.selectionService.showHideDevInfo = true;
+    } else if (this.selectionService.showHideDevInfo == true) {
+      console.log("option menu hidden");
+      this.selectionService.showHideDevInfo = false;
+    }
+  }
+
 
 
 

68cb50e68a80844619687355ed813d5846f9105d
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-10-24 21:53:32 +1100

    Modified API Inputs/outputs. Amended Startup.cs

--- RPS_API/Startup.cs
+++ RPS_API/Startup.cs
@@ -2,14 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 
 namespace RPS_API
 {
@@ -26,6 +27,20 @@ namespace RPS_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(options =>
+                {
+                    options
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                    // I had to disable this to stop the "NullInjectorError: No provider for HttpClient!" error.
+                    // this is not required if we are using .AllowAnyOrigin()
+                    // .AllowCredentials();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,8 +50,11 @@ namespace RPS_API
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors();
 
-            app.UseHttpsRedirection();
+            // I had to disable this to stop the "app.UseHttpsRedirection();
+            // It would not send a request back through https://localhost:5001 otherwise.
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 

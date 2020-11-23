faa68589e8d8473baa9950369b10049ff8c1b1de
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-11-23 12:10:09 +1100

    added "Now Loading" feature to Leaderboard while retreaving results.

--- RockPaperScissorsProject_Ang/src/app/services/leaderboard.service.ts
+++ RockPaperScissorsProject_Ang/src/app/services/leaderboard.service.ts
@@ -10,17 +10,22 @@ import { UsernameRequest, LeaderboardResponse } from "../models/Leaderboard";
 export class LeaderboardService {
 
   LeaderboardItem: LeaderboardResponse[] = [];
+  public loading: boolean = false;
+  public loaded: boolean = false;
 
   constructor(private router: Router, private httpClient: HttpClient) { }
 
   ngOnInit(): void { }
 
   Leaderboard() {
-    let request = this.httpClient.get<LeaderboardResponse[]>("http://localhost:5000/Api/GetLeaderboard",
+    let request = this.httpClient.get<LeaderboardResponse[]>("http://localhost:5000/Api/GetLeaderboard");
     // let request = this.httpClient.post<GameResult>("http://Rpsapi-env-1.eba-jc4wmqcm.us-east-1.elasticbeanstalk.com/Leaderboard",
-    {});
+    this.loading = true;
+    this.loaded = false;
     request.subscribe((response) => {
       this.LeaderboardItem = response;
+      this.loading = false;
+      this.loaded = true;
       this.router.navigateByUrl('/leaderboard');
     }
     );
